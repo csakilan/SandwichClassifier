47427a1a4216c3d71c1c6eb7e63404a54bf19833
AK-Foundry <akilanchithrapro@gmail.com>
2025-11-30 18:19:27 -0600

    Update AWSConnectors.py

--- AWSConnectors.py
+++ AWSConnectors.py
@@ -5,7 +5,7 @@ from inference_sdk import InferenceHTTPClient
 import os
 import time
 #hi guys
-# helo 
+# heo 
 # Configuration - Update these names to match your AWS resources
 S3_BUCKET_NAME = "default-images-1asdfx"
 DYNAMODB_TABLE_NAME = "default-storage1asdfx"
